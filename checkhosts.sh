849bd0b4fcdf3712784cbff4601d1d7eca5b57c5
Alin Iacob <aliniacob13@yahoo.com>
2024-11-23 13:27:51 +0200

    modified:   checkhosts.sh

--- checkhosts.sh
+++ checkhosts.sh
@@ -1,13 +1,22 @@
 #!/bin/bash
 
+validate_ip() {
+    local name=$1
+    local ip=$2
+    local dns_server=$3
+
+    resolved_ip=$(nslookup "$name" "$dns_server" 2>/dev/null | grep -A1 "Name:" | grep "Address:" | tail -n1 | awk '{print $2}')
+
+    if [[ "$resolved_ip" != "$ip" && -n "$resolved_ip" ]]; then
+        echo "Bogus IP for $name in /etc/hosts!"
+    fi
+}
+
 cat /etc/hosts | while read -r ip name; do
     if [[ -z "$ip" || "$ip" == \#* ]]; then
         continue
     fi
 
-    resolved_ip=$(nslookup "$name" 2>/dev/null | grep -A1 "Name:" | grep "Address:" | tail -n1 | awk '{print $2}')
-    
-    if [[ "$resolved_ip" != "$ip" && -n "$resolved_ip" ]]; then
-        echo "Bogus IP for $name in /etc/hosts!"
-    fi
+    validate_ip "$name" "$ip" "8.8.8.8"
 done
+
